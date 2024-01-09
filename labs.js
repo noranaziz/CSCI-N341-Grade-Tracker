e1ed1ab6bdfc8d8cce46a1588a0f2333b282915f
noranaziz <noranaziz18@icloud.com>
2024-01-09 16:37:26 -0500

    edited button colors

--- labs.js
+++ labs.js
@@ -23,12 +23,15 @@ function callAll() {
     // apply random pastel color
     applyRandomPastel(currentPageName)
 
-    // clear localStorage when user is about to leave the page
-    if (currentFileName == 'index.html') {
-        window.addEventListener('beforeunload', function() {
-            localStorage.clear()
-        })
-    }
+    // apply color to buttons in index.html
+    document.getElementById('lab0').style.backgroundColor = getOrCreatePastel('lab0')
+    document.getElementById('lab1').style.backgroundColor = getOrCreatePastel('lab1')
+    document.getElementById('lab2').style.backgroundColor = getOrCreatePastel('lab2')
+    document.getElementById('lab3').style.backgroundColor = getOrCreatePastel('lab3')
+    document.getElementById('midterm').style.backgroundColor = getOrCreatePastel('midterm')
+    document.getElementById('lab4').style.backgroundColor = getOrCreatePastel('lab4')
+    document.getElementById('lab5').style.backgroundColor = getOrCreatePastel('lab5')
+    document.getElementById('final').style.backgroundColor = getOrCreatePastel('final')
 }
 
 // function to get the current file name from the URL
